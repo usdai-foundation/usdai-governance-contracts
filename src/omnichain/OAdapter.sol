fa6b222bf6b82a6de810df5f7a5c9b5c0026dbcc
Ivan A. Sergeev <ivan@permianlabs.xyz>
2026-02-19 01:10:54 -0600

    src/omnichain: add OToken and OAdapter

---
+++ src/omnichain/OAdapter.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.33;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import {OFTCore, IOFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTCore.sol";
+import {RateLimiter} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/utils/RateLimiter.sol";
+
+import {IMintableBurnable} from "../interfaces/IMintableBurnable.sol";
+
+/**
+ * @title Omnichain Adapter
+ * @author Permian Labs
+ */
+contract OAdapter is OFTCore, RateLimiter {
+    /*------------------------------------------------------------------------*/
+    /* Constants */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @notice Implementation version
+     */
+    string public constant IMPLEMENTATION_VERSION = "1.0";
+
+    /*------------------------------------------------------------------------*/
+    /* Immutable state */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @notice Token
+     */
+    IMintableBurnable internal immutable _token;
+
+    /*------------------------------------------------------------------------*/
+    /* Constructor */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @dev OAdapter contructor
+     * @param token_ Wrapped token address
+     * @param lzEndpoint_ LayerZero endpoint address
+     * @param delegate_ Delegate/owner address
+     */
+    constructor(
+        address token_,
+        address lzEndpoint_,
+        address delegate_
+    ) OFTCore(IERC20Metadata(token_).decimals(), lzEndpoint_, delegate_) Ownable(delegate_) {
+        /* Set token */
+        _token = IMintableBurnable(token_);
+    }
+
+    /*------------------------------------------------------------------------*/
+    /* Overrides */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @inheritdoc OFTCore
+     */
+    function _debit(
+        address _from,
+        uint256 _amountLD,
+        uint256 _minAmountLD,
+        uint32 _dstEid
+    ) internal override returns (uint256 amountSentLD, uint256 amountReceivedLD) {
+        (amountSentLD, amountReceivedLD) = _debitView(_amountLD, _minAmountLD, _dstEid);
+        _checkAndUpdateRateLimit(_dstEid, amountSentLD);
+        _token.burn(_from, amountSentLD);
+    }
+
+    /**
+     * @inheritdoc OFTCore
+     */
+    function _credit(
+        address _to,
+        uint256 _amountLD,
+        uint32 /*_srcEid*/
+    ) internal override returns (uint256 amountReceivedLD) {
+        _token.mint(_to, _amountLD);
+        return _amountLD;
+    }
+
+    /*------------------------------------------------------------------------*/
+    /* Getters */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @inheritdoc IOFT
+     */
+    function token() public view override returns (address) {
+        return address(_token);
+    }
+
+    /**
+     * @inheritdoc IOFT
+     */
+    function approvalRequired() external pure returns (bool) {
+        return false;
+    }
+
+    /*------------------------------------------------------------------------*/
+    /* Admin API */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @dev Sets the rate limits based on RateLimitConfig array. Only callable by the owner.
+     * @param _rateLimitConfigs An array of RateLimitConfig structures defining the rate limits.
+     */
+    function setRateLimits(
+        RateLimitConfig[] calldata _rateLimitConfigs
+    ) external onlyOwner {
+        _setRateLimits(_rateLimitConfigs);
+    }
+}
---
+++ src/omnichain/OToken.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.33;
+
+import {ReentrancyGuardTransient} from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
+
+import {ERC20Upgradeable} from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import {
+    ERC20PermitUpgradeable
+} from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {
+    AccessControlUpgradeable
+} from "openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import {MulticallUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/utils/MulticallUpgradeable.sol";
+
+import {IMintableBurnable} from "../interfaces/IMintableBurnable.sol";
+
+/**
+ * @title Omnichain Token
+ * @author Permian Labs
+ */
+contract OToken is
+    IMintableBurnable,
+    ERC20Upgradeable,
+    ERC20PermitUpgradeable,
+    ReentrancyGuardTransient,
+    AccessControlUpgradeable,
+    MulticallUpgradeable
+{
+    /*------------------------------------------------------------------------*/
+    /* Constants */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @notice Minter role
+     */
+    bytes32 public constant BRIDGE_ADMIN_ROLE = keccak256("BRIDGE_ADMIN_ROLE");
+
+    /*------------------------------------------------------------------------*/
+    /* Constructor */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @notice Omnichain Token Constructor
+     */
+    constructor() {
+        _disableInitializers();
+    }
+
+    /*------------------------------------------------------------------------*/
+    /* Initializer */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @notice Initialize the contract
+     * @param name_ Token name
+     * @param symbol_ Token symbol
+     * @param admin Default admin address
+     */
+    function initialize(
+        string memory name_,
+        string memory symbol_,
+        address admin
+    ) public initializer {
+        __ERC20_init(name_, symbol_);
+        __ERC20Permit_init(name_);
+        __Multicall_init();
+        __AccessControl_init();
+
+        /* Grant roles */
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    }
+
+    /*------------------------------------------------------------------------*/
+    /* Minter API */
+    /*------------------------------------------------------------------------*/
+
+    /**
+     * @inheritdoc IMintableBurnable
+     */
+    function mint(
+        address to,
+        uint256 amount
+    ) external onlyRole(BRIDGE_ADMIN_ROLE) nonReentrant {
+        _mint(to, amount);
+    }
+
+    /**
+     * @inheritdoc IMintableBurnable
+     */
+    function burn(
+        address from,
+        uint256 amount
+    ) external onlyRole(BRIDGE_ADMIN_ROLE) nonReentrant {
+        _burn(from, amount);
+    }
+}
